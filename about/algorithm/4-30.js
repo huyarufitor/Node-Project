4e33bc92e0cee6614e4dc5be81f749707150620e
fitorhu <fitorhu@zego.im>
2021-05-18 10:08:50 +0800

    feat：node start

---
+++ about/algorithm/4-30.js
@@ -0,0 +1,8 @@
+var nums =[2,4,4,2];
+var singleNumber = function(nums) {
+    for(const i=0;i<nums.length;i++){
+        const sum =0;
+        sum +=nums[i];
+    }
+    return sum;
+};
---
+++ about/helloworld.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port  = 315;
+const server = http.createServer
